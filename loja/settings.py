d3432a39ead4b5b69d3bca2a09fb164245c4976c
Russell Cavalcante <russell.cavalcante.2882@gmail.com>
2023-11-20 14:42:07 -0300

    atualizado os settings para aceitar todos os hosts

--- loja/settings.py
+++ loja/settings.py
@@ -83,13 +83,23 @@ WSGI_APPLICATION = 'loja.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'dpg-cldpig8hgaic73bm648g-a.oregon-postgres.render.com',
+        'NAME': 'eccomerce_db',
+        'PORT': 5432,
+        'USER': 'admin',
+        'PASSWORD': 'qZjyOIstTer0SN2km0M1x4S821diCPXH   '
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
